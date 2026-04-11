4da886bbedc4ea9aa68332045582949718579b91
Mugara250 <mbonyumugara52@gmail.com>
2026-04-12 00:26:21 +0200

    create: login button

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -4,16 +4,28 @@ import Navbar from '../components/Navbar';
 
 function LandingPage() {
   return (
-    <div className="h-screen  bg-cover bg-center" style={{ backgroundImage: `url(${carImage})` }}>
+    <div
+      className="h-screen relative text-white bg-cover bg-center"
+      style={{ backgroundImage: `url(${carImage})` }}>
+      <div className="absolute inset-0 bg-black/60"></div>
       <Navbar />
-      <h1 className="text-4xl md:text-6xl font-bold mb-4">
-        <TypeAnimation
-          sequence={['Rent.', 800, 'Ride.', 800, 'Repeat.', 1200]}
-          speed={50}
-          repeat={Infinity}
-          wrapper="span"
-        />
-      </h1>
+      <div className="mt-40 relative flex flex-col justify-center items-center gap-y-4">
+        <h1 className="text-4xl font-extrabold">DriveEase</h1>
+        <h1 className="text-4xl  font-bold">
+          <TypeAnimation
+            sequence={['Rent', 800, 'Ride', 800, 'Repeat', 1200]}
+            speed={50}
+            repeat={Infinity}
+            wrapper="span"
+          />
+        </h1>
+        <p className="text-2xl font-semibold">Find your perfect car in seconds.</p>
+        <button
+          type="button"
+          className="mt-5 bg-white text-[#265E78] text-2xl font-semibold px-36 py-2 rounded-3xl hover:bg-[#265E78] hover:text-white">
+          Login
+        </button>
+      </div>
     </div>
   );
 }
