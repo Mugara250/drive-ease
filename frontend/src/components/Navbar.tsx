280f5a79811d359c490a827196ad1dab58e3898c
Mugara250 <mbonyumugara52@gmail.com>
2026-04-11 21:29:20 +0200

    create: login-logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,10 +2,11 @@ import logo from '../assets/logo header.png';
 
 function Navbar() {
   return (
-    <div className="bg-[#111827]">
+    <div className="relative bg-[#111827] h-[4.2rem] flex justify-center rounded-b-xl">
       <div>
-        <img src={logo} alt="application logo" />
+        <img src={logo} alt="application logo" className="size-20 object-cover" />
       </div>
+      <div className="absolute bg-inherit size-24 -z-10 -bottom-5 rounded-full"></div>
     </div>
   );
 }
